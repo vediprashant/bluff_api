30b0a534089209ad924d0a4861b181429bb7bea4
Prashant <prashant.chaturvedi@joshtechnologygroup.com>
2021-01-27 17:22:49 +0530

    updates celery timezone

--- bluffapi/bluffapi/settings/base.py
+++ bluffapi/bluffapi/settings/base.py
@@ -145,7 +145,7 @@ CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
-CELERY_TIMEZONE = 'Asia/Delhi'
+CELERY_TIMEZONE = 'Asia/Dhaka'
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
